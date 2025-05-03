2349195c8425f979362a7a2c0e880e5d85580e3e
Habib Tanwir <habibtanwir1906@gmail.com>
2025-05-03 14:29:06 +0530

    Update klicktape submodule

---
+++ public/config.js
@@ -0,0 +1,4 @@
+const SUPABASE_CONFIG = {
+    url: 'https://rllllgeukajuwfansqym.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJsbGxsZ2V1a2FqdXdmYW5zcXltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyMTQ4NjEsImV4cCI6MjA2MDc5MDg2MX0.dMT33vvnsBH2AL86hJIxsfJ0cJaQfu2N1ND6vOiDRv4'
+};
